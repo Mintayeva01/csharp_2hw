41abc8e9db3b34b2394459afddbe2a39c1d31613
Пользователь <mintayeva01@mail.ru>
2022-12-04 20:43:31 +0600

    all files added

---
+++ Task03/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите день недели");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+void TheDayOfTheWeek (int dayNumber)
+    {
+  if (dayNumber == 6 || dayNumber == 7)
+
+    {
+  Console.WriteLine("этот день выходной");
+    }
+  
+    else 
+
+    {
+  Console.WriteLine("этот день не выходной");
+    }
+    }
+    TheDayOfTheWeek(dayNumber);
